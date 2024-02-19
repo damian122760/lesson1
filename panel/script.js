e5a1c51daffebf48f8e9271873306cd3e62e7b74
Damian <damian122760@interia.eu>
2024-02-19 21:17:05 +0100

    Co-authored-by: Adam Jochemczyk <adamjoche@gmail.com>

--- panel/script.js
+++ panel/script.js
@@ -2,14 +2,20 @@ const table = document.querySelector('table');
 const thead = document.querySelector('thead');
 const tbody = table.querySelector('tbody');
 const trAll = document.getElementsByTagName('tr');
-const URL = 'https://jsonplaceholder.typicode.com/comments';
-const URL2 = 'https://jsonplaceholder.typicode.com/posts';
-const check = document.querySelector('.showForbidden');
+const baseAPIUrl = 'https://jsonplaceholder.typicode.com/';
+const commentsEndpoint = `${baseAPIUrl}/comments`;
+const postsEndpoint = `${baseAPIUrl}/posts`;
+
+const checkShowForbidden = document.querySelector('.showForbidden');
 const input = document.querySelector('.searchInput');
 let startIndex = 0;
+
+const COMMENTS_PER_PAGE = 20;
 const arrowLeft = document.querySelector('.fa-arrow-left');
 const arrowRight = document.querySelector('.fa-arrow-right');
 const select = document.getElementById('select');
+
+const WRONG_WORD = 'quam';
 
 function removeRow() {
 	const checkboxes = document.querySelectorAll(
@@ -17,13 +23,15 @@ function removeRow() {
 	);
 	checkboxes.forEach((checkbox) => {
 		const row = checkbox.parentNode.parentNode;
-		const children = row.children[3];
+		const indexOfCommentBody = 3;
+		const children = row.children[indexOfCommentBody];
 		console.log(children.textContent);
 		row.remove();
 	});
 }
-function displayTable(data) {
-	for (let i = startIndex; i < startIndex + 20; i++) {
+
+function generateRows(data) {
+	for (let i = startIndex; i < startIndex + COMMENTS_PER_PAGE; i++) {
 		const tr = document.createElement('tr');
 		tr.setAttribute('id', data[i]['id']);
 		tr.innerHTML = `<td class="firstTd">${data[i]['id']}</td>
@@ -31,25 +39,75 @@ function displayTable(data) {
     <td class="email">${data[i]['email']}</td>
     <td class="body">${data[i]['body']}</td>
     <td class="action"><button class="btns">Usuń</button>
-    <label for="check">Zaznacz komentarz, by usunąć</label>
-    <input type="checkbox" class="deleteCheck" name="check"/> 
+    <label for="checkShowForbidden">Zaznacz komentarz, by usunąć</label>
+    <input type="checkbox" class="deleteCheck" name="checkShowForbidden"/> 
     </td>`;
 		tbody.appendChild(tr);
 	}
-	const buttons = document.querySelectorAll('.btns');
-	buttons.forEach((button) => {
-		button.addEventListener('click', removeRow);
-	});
+}
+
+// pure functions
+
+// nie mają zależności zewnętrznych
+
+//pure function
+// const test=(a,b)=>a+b;
+
+// a to nie pure
+// const test2=new Date()
+
+// const test4=(a)=>a*test2.getMilliseconds()
+
+function searchPhrase(tdName, tdEmail, tdBody) {
+			input.addEventListener('input', () => {
+				if (
+					(tdName.textContent
+						.toLowerCase()
+						.includes(input.value.toLowerCase()) ||
+						tdEmail.textContent
+							.toLowerCase()
+							.includes(input.value.toLowerCase()) ||
+						tdBody.textContent.toLowerCase().includes(input.value)) &&
+					input.value != ''
+				) {
+					tr.style.background = 'rgb(105, 29, 252)';
+				} else if (input.value == '') {
+					tr.style.background = 'rgba(0, 0, 0, 0.2)';
+				} else {
+					tr.style.background = 'rgba(0, 0, 0, 0.2)';
+				}
+			});
+		}
+
+function displayTable(data) {
+	generateRows(data);
+	//TODO: do osbnej funkcji :done
+
+	function generateRowDeleteButtons() {
+		const buttons = document.querySelectorAll('.btns');
+		buttons.forEach((button) => {
+			button.addEventListener('click', removeRow);
+		});
+	}
+	generateRowDeleteButtons();
+
 	Array.from(trAll).forEach((tr) => {
-		const children1 = tr.children[1];
-		const children2 = tr.children[2];
-		const children3 = tr.children[3];
-		check.addEventListener('click', fun1);
-		input.addEventListener('input', fun2);
-		function fun1() {
+		//TODO: magic numbe
+
+		//TODO: nazewnictwo done
+		const tdName = tr.children[1];
+		const tdEmail = tr.children[2];
+		const tdBody = tr.children[3];
+
+		//TODO: nazewnictwo: done
+		checkShowForbidden.addEventListener('click', () =>
+			colorRow(tdName, tdBody, tr)
+		);
+		//TODO: przerobić tak żeby fun2 było pure  done: (fun2 przerobiłem na searchPhrase)
+		function colorRow(tdName, tdBody, tr) {
 			if (
-				(children1.textContent.includes('quam') ||
-					children3.textContent.includes('quam')) &&
+				(tdName.textContent.includes(WRONG_WORD) ||
+					tdBody.textContent.includes(WRONG_WORD)) &&
 				tr.style.background != 'goldenrod'
 			) {
 				tr.style.background = 'goldenrod';
@@ -57,74 +115,67 @@ function displayTable(data) {
 				tr.style.background = 'rgba(0, 0, 0, 0.2)';
 			}
 		}
-		function fun2() {
-			if (
-				(children1.textContent
-					.toLowerCase()
-					.includes(input.value.toLowerCase()) ||
-					children2.textContent
-						.toLowerCase()
-						.includes(input.value.toLowerCase()) ||
-					children3.textContent.toLowerCase().includes(input.value)) &&
-				input.value != ''
-			) {
-				tr.style.background = 'rgb(105, 29, 252)';
-			} else if (input.value == '') {
-				tr.style.background = 'rgba(0, 0, 0, 0.2)';
-			} else {
-				tr.style.background = 'rgba(0, 0, 0, 0.2)';
-			}
-		}
+
+		
+		searchPhrase(tdName, tdEmail, tdBody);
 	});
+
+	function showNextComments() {
+		if (startIndex > 0) {
+			tbody.innerHTML = ``;
+			startIndex -= COMMENTS_PER_PAGE;
+			displayTable(data);
+		}
+	}
+	function showPreviousComments() {
+		if (startIndex < data.length - COMMENTS_PER_PAGE) {
+			tbody.innerHTML = ``;
+			startIndex += COMMENTS_PER_PAGE;
+			displayTable(data);
+		}
+	}
+	arrowLeft.addEventListener('click', showNextComments);
+	arrowRight.addEventListener('click', showPreviousComments);
 }
 
-fetch(URL)
+//start
+let commentsEndpointData;
+fetch(commentsEndpoint)
 	.then((res) => res.json())
 	.then((data) => {
-		displayTable(data);
-		function showNextComments() {
-			if (startIndex > 0) {
-				tbody.innerHTML = ``;
-				startIndex -= 20;
-				displayTable(data);
-			}
-		}
-		function showPreviousComments() {
-			if (startIndex < data.length - 20) {
-				tbody.innerHTML = ``;
-				startIndex += 20;
-				displayTable(data);
-			}
-		}
-		arrowLeft.addEventListener('click', showNextComments);
-		arrowRight.addEventListener('click', showPreviousComments);
+		commentsEndpointData = data;
+		//TODO: data do globalnej zmiennej: done
+
+		displayTable(commentsEndpointData);
 	});
 
-fetch(URL2)
+
+let postsEndpointData;
+fetch(postsEndpoint)
 	.then((res) => res.json())
 	.then((data) => {
-		function fun1(data) {
-			for (i = 0; i < data.length; i++) {
+		//TODO: nazewnictwo: done
+		postsEndpointData = data;
+		function loadOptions() {
+			for (i = 0; i < postsEndpointData.length; i++) {
 				const option = document.createElement('option');
-				option.setAttribute('value', data[i]['id']);
-				option.textContent = data[i]['title'];
+				option.setAttribute('value', postsEndpointData[i]['id']);
+				option.textContent = postsEndpointData[i]['title'];
 				select.appendChild(option);
 			}
 		}
-		fun1(data);
-		select.addEventListener('change', fun2);
-		function fun2() {
+
+		function showPosts() {
 			const selectedOption = select.value;
-			fetch(URL2)
-				.then((res) => res.json())
-				.then((data) => {
-					thead.innerHTML = '';
-					tbody.innerHTML = '';
-					const tr = document.createElement('tr');
-					tr.innerHTML = `<td class="body">${
-						data[selectedOption - 1]['body']
-					}</td>`;
-					tbody.appendChild(tr);
-				});
+			thead.innerHTML = '';
+			tbody.innerHTML = '';
+			const tr = document.createElement('tr');
+			tr.innerHTML = `<td class="body">${
+				postsEndpointData[selectedOption - 1]['body']
+			}</td>`;
+			tbody.appendChild(tr);
 		}
+		
+		loadOptions(postsEndpointData);
+		select.addEventListener('change', showPosts);
 	});
